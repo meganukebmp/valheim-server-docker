a25580f997b83079ab0f433681f1a50a9d48d5df
Nexrem <kristiana2000@abv.bg>
2021-02-17 18:55:01 +0200

    Drop env_flag for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,8 @@ export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
 
-if [ -z "$VH_SERVER_FLAGS" ]
-then
-	/home/steamuser/valheim_server/valheim_server.x86_64 -name "My Server" -port 2456 -world "Dedicated" -password "secret" -savedir "/home/steamuser/valheim_data"
-else
-	/home/steamuser/valheim_server/valheim_server.x86_64 $VH_SERVER_FLAGS
-fi
+/home/steamuser/valheim_server/valheim_server.x86_64 -name "My Server" -port 2456 -world "Dedicated" -password "secret" -savedir "/home/steamuser/valheim_data"
+
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
